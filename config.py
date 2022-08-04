db40f76a5c6aea87f863bf697ababebd19c89ee4
babucarr32 <93452510+babucarr32@users.noreply.github.com>
2022-08-04 10:00:12 +0000

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-CHROME_PROFILE_PATH = r"user-data-dir=C:\Users\USER\AppData\Local\Google\Chrome\User Data\TheNameOfYourFolder"
+CHROME_PROFILE_PATH = r"user-data-dir=C:\Users\<username>\AppData\Local\Google\Chrome\User Data\<TheNameOfYourFolder>"
 
 # Windows 7, 8.1, and 10: C:\Users\<username>\AppData\Local\Google\Chrome\User Data\Default
 # Mac OS X El Capitan: Users/<username>/Library/Application Support/Google/Chrome/Default
