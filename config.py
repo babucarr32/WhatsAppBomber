a25988c01f067a0e8ae43154497cb29c127e0fb2
babucarr32 <baboucarry@gmail.com>
2023-02-04 23:08:23 +0000

    Added the hint for users to edit based on their OS

--- config.py
+++ config.py
@@ -3,8 +3,10 @@ import os, platform
 platformFinder = platform.system()
 
 if platformFinder == "Windows":
+    # Edit this if your are running Windows.
     CHROME_PROFILE_PATH = r"user-data-dir=C:\Users\<username>\Name\Of\Folder\To\Store\Data"
 else:
+    # Edit this if your are running Linux.
     CHROME_PROFILE_PATH = r"user-data-dir=/home/<username>/Name/Of/Folder/To/Store/Data"
 
 # Windows 7, 8.1, and 10: C:\Users\<username>\AppData\Local\Google\Chrome\User Data\Default
